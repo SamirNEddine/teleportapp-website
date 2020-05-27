acb3241e31287bcb7e8738f043b0ac2c8e95d6b0
Samir <samir@nevereatalone.io>
2020-05-27 15:27:47 +0200

    imrove Slack notification job

--- gcb_slack/index.js
+++ gcb_slack/index.js
@@ -24,12 +24,12 @@ module.exports.subscribeSlack = (pubSubEvent, context) => {
 // eventToBuild transforms pubsub event message to a build object.
 const eventToBuild = (data) => {
   return JSON.parse(Buffer.from(data, 'base64').toString());
-}
+};
 
 // createSlackMessage creates a message from a build object.
 const createSlackMessage = (build) => {
   const message = {
-    text: `Build - Trigger: \`${build.buildTriggerId}\` -  ID: \`${build.id}\``,
+    text: `Repository: \`${build.substitutions.REPO_NAME}\` -  Environment: \`Test\``,
     mrkdwn: true,
     attachments: [
       {
@@ -43,4 +43,4 @@ const createSlackMessage = (build) => {
     ]
   };
   return message;
-}
+};
