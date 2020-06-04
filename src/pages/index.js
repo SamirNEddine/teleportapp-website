b270f7b93fe4bc3905ddc601e342c6abc34d04c1
Samir <samir@nevereatalone.io>
2020-06-04 15:43:53 +0200

    top section - no form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout";
 import {graphql, useStaticQuery} from 'gatsby';
+import HomePageStyles from './homepage.module.css';
+import TopSectionIllustration from '../assets/top-section-illustration.svg';
 
 const productFeaturesQuery = graphql`
     query {
@@ -22,20 +24,38 @@ const ProductPage = function () {
     const {edges: productFeatures} = allContentfulProductFeatureDescription;
     return (
         <Layout>
-            <h1>Hello world</h1>
-            <ul>
-                {
-                    productFeatures.map( featureEdge => {
-                        const {node: feature} = featureEdge;
-                        return(
-                            <li>
-                                <h2>{feature.title}</h2>
-                                <p>{feature.description.description}</p>
-                            </li>
-                        )
-                    })
-                };
-            </ul>
+            <div className={HomePageStyles.topBrandingSectionContainer}>
+                <div className={HomePageStyles.left}>
+                    <ul>
+                        <li><h1>Your remote working assistant</h1></li>
+                        <li><h2>Let your team know how and when is best to reach you in few clicks.</h2></li>
+                        <li>
+                            <form>
+                                <input type='email' placeholder='Your work email'/>
+                                <button>Get early access</button>
+                            </form>
+                        </li>
+
+                    </ul>
+                </div>
+                <div className={HomePageStyles.right}>
+                    <img src={TopSectionIllustration} alt='top section illustration'/>
+                </div>
+            </div>
+            {/*<h1>Hello world</h1>*/}
+            {/*<ul>*/}
+            {/*    {*/}
+            {/*        productFeatures.map( featureEdge => {*/}
+            {/*            const {node: feature} = featureEdge;*/}
+            {/*            return(*/}
+            {/*                <li>*/}
+            {/*                    <h2>{feature.title}</h2>*/}
+            {/*                    <p>{feature.description.description}</p>*/}
+            {/*                </li>*/}
+            {/*            )*/}
+            {/*        })*/}
+            {/*    };*/}
+            {/*</ul>*/}
         </Layout>
     )
 };
