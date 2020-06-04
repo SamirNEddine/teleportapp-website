b124b7efb7049fdb7777543b40e0f4fb561651ac
Samir <samir@nevereatalone.io>
2020-06-04 11:46:06 +0200

    add top menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link, graphql, useStaticQuery} from "gatsby";
-
+import TeleportLogo from '../../assets/teleport-logo-white.png';
 import HeaderStyles from './header.module.css';
 
 const titleQuery = graphql`
@@ -17,12 +17,12 @@ const Header = function () {
 
     return(
         <div className={HeaderStyles.container}>
-            {data.site.siteMetadata.title}  
-            <ul className={HeaderStyles.navigationList}>
-                <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/">Product</Link></li>
-                <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/consulting">Consulting</Link></li>
-                <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/about">About</Link></li>
-            </ul>
+            <img src={TeleportLogo} className={HeaderStyles.teleportLogo} alt='Teleport logo'/>
+            {/*<ul className={HeaderStyles.navigationList}>*/}
+            {/*    <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/">Product</Link></li>*/}
+            {/*    <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/consulting">Consulting</Link></li>*/}
+            {/*    <li><Link className={HeaderStyles.navigationItem} activeClassName={HeaderStyles.selectedNavigationItem} to="/about">About</Link></li>*/}
+            {/*</ul>*/}
         </div>
     )
 };
