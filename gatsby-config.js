34ffd48a536410820941bccf84c099f812b6d13c
Samir <samir@nevereatalone.io>
2020-06-07 22:25:17 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,18 @@ module.exports = {
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
                 environment: process.env.CONTENTFUL_ENVIRONMENT
             }
-        }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Teleport`,
+                short_name: `Teleport`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/assets/teleport-logo-mark-full-colour-rgb.png`
+            },
+        },
     ]
 };
