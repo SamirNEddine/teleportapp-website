9b8e292e8be24b9edc0623ee02702ac45d2398af
Samir <samir@nevereatalone.io>
2020-06-08 19:33:25 +0200

    add createPage gatsby method

---
+++ i18n/supported-languages.js
@@ -0,0 +1,11 @@
+module.exports = {
+    en: {
+        is_default: true,
+        path: 'en',
+        display_name: 'English',
+    },
+    fr: {
+        path: 'fr',
+        display_name: 'Français',
+    }
+};
