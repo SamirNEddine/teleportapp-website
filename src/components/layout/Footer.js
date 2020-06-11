a0db59b087d70a4c05251d94b0aefa6989a37430
Samir <samir@nevereatalone.io>
2020-06-11 10:10:56 +0200

    add outbound link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import WithTranslations from '../i18n/withTranslations';
 import i18n from "../i18n/config"
 import {LocalizedLink} from '../i18n/localizedLink';
+import {OutboundLink} from 'gatsby-plugin-gtag'
 import FooterStyles from './footer.module.css';
 import TeleportLogo from '../../assets/teleport-logo-full-colour.svg';
 import ContactMail from '../../assets/contact-mail.svg';
@@ -27,9 +28,9 @@ const Footer = function ({language}) {
                 <div className={FooterStyles.contactContainer}>
                     <h2 className={FooterStyles.sectionTitle}>{i18n.t('Footer - Created by a fully remote team')}</h2>
                     <ul>
-                        <li><a target="_blank" href='https://twitter.com/teleport_io'><img src={ContactTwitter} alt='Twitter page'/></a></li>
-                        <li><a target="_blank" href='https://www.linkedin.com/company/tlprt/'><img src={ContactLinkedIn} alt='Linked page'/></a></li>
-                        <li><a target="_blank" href='mailto:contact@teleport.so'><img src={ContactMail} alt='Contact by email'/></a></li>
+                        <li><OutboundLink target="_blank" href='https://twitter.com/teleport_io'><img src={ContactTwitter} alt='Twitter page'/></OutboundLink></li>
+                        <li><OutboundLink target="_blank" href='https://www.linkedin.com/company/tlprt/'><img src={ContactLinkedIn} alt='Linked page'/></OutboundLink></li>
+                        <li><OutboundLink target="_blank" href='mailto:contact@teleport.so'><img src={ContactMail} alt='Contact by email'/></OutboundLink></li>
                     </ul>
                 </div>
             </div>
