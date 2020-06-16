c05b2838e389d81cc58ea7b7180b8446a9d3eb41
Samir <samir@nevereatalone.io>
2020-06-16 11:30:05 +0200

    terms page

---
+++ src/pages/terms.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Layout from "../components/layout";
+import {graphql, useStaticQuery} from 'gatsby';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Styles from './legal.module.css';
+import SEO from "../components/seo"
+import i18n from "../components/i18n/config";
+
+const termsQuery = graphql`
+    query {
+        contentfulLegalInformation(title: {eq: "Terms"}) {
+            content {
+                json
+            }
+        }
+    }
+`;
+
+const TermsPage = function ({language}) {
+    const {contentfulLegalInformation: termsNode} = useStaticQuery(termsQuery);
+
+    return (
+        <Layout>
+            <SEO title={i18n.t('Terms | Teleport')} description={i18n.t('Terms')} lang={language} />
+            <div className={Styles.container}>
+                {documentToReactComponents(termsNode.content.json)}
+            </div>
+        </Layout>
+    )
+};
+export default TermsPage;
